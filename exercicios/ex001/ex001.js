139c30a49413408dde1f6b4918e97810f9b154c8
Caio Costa <caiopaulinocosta@hotmail.com>
2024-04-10 19:00:56 -0300

    Adiciona ex001
    Adicionando resolução do primeiro exercício.

---
+++ exercicios/ex001/ex001.js
@@ -0,0 +1,6 @@
+var n1 = 5;
+var n2 = 10;
+function sum(n1, n2) {
+    console.log(n1 + n2);
+}
+sum(n1, n2);
---
+++ exercicios/ex001/ex001.ts
@@ -0,0 +1,8 @@
+const n1 = 5
+const n2 = 10
+
+function sum (n1: number, n2: number) {
+    console.log(n1+n2)
+}
+
+sum(n1, n2)
